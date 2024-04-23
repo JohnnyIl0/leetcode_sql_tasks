4bddc6e8722b311a6c6dc8e78092556eb6d034f4
JohnnyIl0 <158089324+JohnnyIl0@users.noreply.github.com>
2024-04-23 21:02:29 +0300

    Basic Aggregate Functions

---
+++ SQL 50/Basic Aggregate Functions/Percentage of Users Attended a Contest.sql
@@ -0,0 +1,36 @@
+-- Table: Users
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | user_id     | int     |
+-- | user_name   | varchar |
+-- +-------------+---------+
+-- user_id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the name and the id of a user.
+ 
+
+-- Table: Register
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | contest_id  | int     |
+-- | user_id     | int     |
+-- +-------------+---------+
+-- (contest_id, user_id) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table contains the id of a user and the contest they registered into.
+ 
+
+-- Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+-- Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+-- The result format is in the following example.
+SELECT r.contest_id, round((count(r.contest_id)/(select COUNT(*) from Users)* 100), 2) as percentage 
+FROM users u
+JOIN register r
+    on u.user_id = r.user_id
+GROUP BY r.contest_id
+HAVING r.contest_id IS NOT NULL
+order by percentage desc, r.contest_id
